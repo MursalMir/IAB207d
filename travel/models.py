3af990ff61e6ea4f17d5ddc6eb1bf95635046257
MursalMir <146822678+MursalMir@users.noreply.github.com>
2023-10-18 17:22:20 +1000

    Update models.py

--- travel/models.py
+++ travel/models.py
@@ -27,7 +27,6 @@ class Destination(db.Model):
     # ... Create the Comments db.relationship
 	# relation to call destination.comments and comment.destination
     comments = db.relationship('Comment', backref='destination')
-
 	# string print method
     def __repr__(self):
         return f"Name: {self.name}"
@@ -41,6 +40,43 @@ class Comment(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     destination_id = db.Column(db.Integer, db.ForeignKey('destinations.id'))
 
-    # string print method
     def __repr__(self):
         return f"Comment: {self.text}"
+    
+class Hotel(db.Model):
+    __tablename__ = 'hotels'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), index=True, nullable=False)
+    description = db.Column(db.String(500))
+    room_avail = db.Column(db.Boolean, default=1)
+    # Define the one-to-many relationship with the Room model
+    rooms = db.relationship('Room', backref='hotel', lazy='dynamic')
+    destination_id = db.Column(db.Integer, db.ForeignKey('destinations.id'))
+
+    def to_dict(self):
+        h_dict = {
+            b.name: str(getattr(self, b.name)) for b in self.__table__.columns
+        }
+        h_rooms = []
+        # Add details of related Rooms to the Hotel's h_dict
+        for room in self.rooms:
+            room_data = {
+                'id': room.id,
+                'room_type': room.type,
+                'num_rooms': room.num_rooms,
+                'room_description': room.description,
+                'room_rate': room.rate,
+                'hotel_id': room.hotel_id
+            }
+            h_rooms.append(room_data)
+        h_dict['rooms'] = h_rooms
+        return h_dict
+
+class Room(db.Model):
+    __tablename__ = 'rooms'
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(50), index=True, nullable=False)
+    num_rooms = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(500))
+    rate = db.Column(db.Float(7))
+    hotel_id = db.Column(db.Integer, db.ForeignKey('hotels.id'))
